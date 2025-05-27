6d2be0ad5011cc24212925a79a13a125a77b9849
TechMidia <Techmidia.agencia@gmail.com>
2025-05-27 16:50:01 -0300

    Update imageSearch.js

--- imageSearch.js
+++ imageSearch.js
@@ -1,1 +1,15 @@
+// services/imageSearch.js
+const axios = require('axios');
 
+async function buscarImagemGoogle(produto) {
+  const apiKey = process.env.SERPAPI_KEY;
+  const query = encodeURIComponent(produto);
+
+  const url = `https://serpapi.com/search.json?q=${query}&tbm=isch&api_key=${apiKey}`;
+  const response = await axios.get(url);
+  const imagem = response.data.images_results?.[0]?.original || null;
+
+  return imagem;
+}
+
+module.exports = buscarImagemGoogle;
