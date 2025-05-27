1fdf56ac98d3060b00c65c21e2a753222a765106
TechMidia <Techmidia.agencia@gmail.com>
2025-05-27 16:50:37 -0300

    Update removeBackground.js

--- removeBackground.js
+++ removeBackground.js
@@ -1,1 +1,26 @@
+// services/removeBackground.js
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
 
+async function removerFundoDezgo(imageUrl, nomeArquivo) {
+  const response = await axios.post(
+    'https://api.dezgo.com/remove-background',
+    {
+      image_url: imageUrl,
+    },
+    {
+      headers: {
+        'X-Api-Key': process.env.DEZGO_API_KEY,
+        'Content-Type': 'application/json',
+      },
+      responseType: 'arraybuffer',
+    }
+  );
+
+  const filePath = path.join(__dirname, `../assets/${nomeArquivo}.png`);
+  fs.writeFileSync(filePath, response.data);
+  return filePath;
+}
+
+module.exports = removerFundoDezgo;
