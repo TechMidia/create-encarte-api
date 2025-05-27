9d6c234d50d9bc1d5c7bc07d5dfd5a2a18d8974e
TechMidia <Techmidia.agencia@gmail.com>
2025-05-27 19:50:31 -0300

    Create usuario.js

---
+++ routes/usuario.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const { cadastrarUsuario } = require('../controllers/usuarioController');
+
+router.post('/cadastrar-usuario', async (req, res) => {
+  try {
+    const resultado = await cadastrarUsuario(req.body);
+    res.json(resultado);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ erro: 'Erro ao cadastrar usuário' });
+  }
+});
+
+module.exports = router;
